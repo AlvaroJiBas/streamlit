fc18b02d2871fd157fa7afe9696252a49f68d3d1
AlvaroJiBas <ajimenezba90@gmail.com>
2025-04-04 18:39:47 +0000

    sdfas

--- Progress.py
+++ Progress.py
@@ -1,2 +1,12 @@
 import streamlit as st
+import pandas as pd
+if 'workout' not in st.session_state:
+    st.session_state['workout']= pd.DataFrame(pd.read_csv("workout.csv"))
+if 'data' not in st.session_state:
+     st.session_state['data']= pd.DataFrame(pd.read_csv("BMI.csv"))   
+
 st.title("🏃‍♂️‍➡️Progress Tracker")
+st.subheader("Weight lifted over time")
+st.line_chart(st.session_state['workout'], y="weight")
+st.subheader("🏋️‍♀️BMI over time")
+st.line_chart(st.session_state['data'], y="BMI")

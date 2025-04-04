83c5b168e936006f5a4f4a90347ab01d0688fa8b
AlvaroJiBas <ajimenezba90@gmail.com>
2025-04-04 18:27:41 +0000

    ahora si

--- Workout.py
+++ Workout.py
@@ -10,7 +10,7 @@ reps = st.number_input("Reps",min_value=1,value=1)
 weight = st.number_input("Weight (kg)",min_value=1,value=10)
 if st.button("Log Workout", type="primary" ):
     new_row = {"exercise" : title, "sets" : sets, "reps" : reps, "weight": weight}
-    st.session_state['workout'] = pd.concat([st.session_state['data'], pd.DataFrame([new_row])], ignore_index=True)
+    st.session_state['workout'] = pd.concat([st.session_state['workout'], pd.DataFrame([new_row])], ignore_index=True)
     dataframe =pd.DataFrame( st.session_state['workout'])
     dataframe.to_csv("workout.csv")
 st.subheader("🏋️‍♀️Workout history")
